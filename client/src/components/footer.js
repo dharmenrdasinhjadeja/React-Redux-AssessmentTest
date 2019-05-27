e4e5d40bb15992da22fcddf814e8ee7c4751e47e
DharmendraSinh Jadeja <dharmendrasinh.jadeja@thegatewaycorp.com>
2019-05-27 13:31:48 +0530

    added project code.

---
+++ client/src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <span>@2019 Redux Blog App</span>
+      </div>
+    </footer>
+  );
+}
